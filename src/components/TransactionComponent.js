b8a9170590939f3bf069fac7b9046ae4c7de8680
ShahzaibSE <shahzaibanalyst@outlook.com>
2020-06-20 17:24:22 +0500

    Making list scrollable.

--- src/components/TransactionComponent.js
+++ src/components/TransactionComponent.js
@@ -1,14 +1,28 @@
 import React, {useContext} from 'react'
 import {globalContext} from "../context/GlobalState"
+import {ListItem, Typography, ListItemText, ListItemSecondaryAction,
+        IconButton, makeStyles} from "@material-ui/core"
+import DeleteIcon from '@material-ui/icons/Delete';      
+
 
 export const TransactionComponent = ({transaction}) => {
     let sign = transaction.amount > 0 ? "+" : "-"
     const {deleteTransaction} = useContext(globalContext)
+    //
     return (
-        <li className={transaction.amount < 0 ? 'minus': 'plus'}>
-        {transaction.text} <span>{sign}${Math.abs(transaction.amount)}
-                           </span><button className="delete-btn" onClick={()=>deleteTransaction(transaction.id)}>
-                               x</button>
-      </li>
+        <ListItem>
+            <ListItemText primary={transaction.text}
+                          secondary= {`${sign}$${Math.abs(transaction.amount)}`}/>
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon onClick={()=>deleteTransaction(transaction.id)} />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+        </ListItem>
+        // <li className={transaction.amount < 0 ? 'minus': 'plus'}>
+        // {transaction.text} <span>{sign}${Math.abs(transaction.amount)}
+        //                    </span><button className="delete-btn" onClick={()=>deleteTransaction(transaction.id)}>
+        //                        x</button>
+        // </li>
     )
 }
--- src/components/TransactionListComponent.js
+++ src/components/TransactionListComponent.js
@@ -1,19 +1,44 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import {globalContext} from "../context/GlobalState"
-import {Grid} from "@material-ui/core"
+import {Grid, List, makeStyles} from "@material-ui/core"
 // Component.
 import {TransactionComponent} from "./TransactionComponent"
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+    // position: 'relative',
+    overflow: 'scroll',
+    maxHeight: 250,
+  }
+}));
+
 export const TransactionListComponent = () => {
   const context = useContext(globalContext)
+  const classes = useStyles()
   let transactions = context.transactions
+  // Material list generator.
+  const [dense, setDense] = useState(true)
+  let generate = (element) => {
+    transactions.map((transaction) =>
+      (React.cloneElement(element, {
+        key: transaction,
+      })
+    ));
+  }
     return (
        <Grid container style={{display:"block"}}>
         <Grid item xs={12} lg={12}> 
-        <h3>History</h3>
+         <h3>History</h3>
+         <List dense={dense} className={`${classes.root}`}>
+          {transactions.map(transaction=>(<TransactionComponent transaction={transaction}/>))}
+         </List>
+        {/* <h3>History</h3>
         <ul className="list">
           {transactions.map(transaction=>(<TransactionComponent transaction={transaction}/>))}
-        </ul>
+        </ul> */}
         </Grid>
       </Grid>
     )
